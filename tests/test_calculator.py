98cd3ac9c113147abba398fe55627f4a7a5bfb8a
Julio QM <julioquinchichester@gmail.com>
2025-09-27 11:28:32 -0600

    cambio name po main

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -8,5 +8,5 @@ class TestCalculator(unittest.TestCase):
     def test_resta(self):
         self.assertEqual(resta(5,3),2)
 
-if __name__== '__name__':
+if __name__== '__main__':
     unittest.main()
